91bb503efdb714ae1d36ce960e165d2500ad986e
wangbz09 <wangbz@189.cn>
2018-02-04 07:14:59 +0800

    fix the bug of second link

--- wbz.js
+++ wbz.js
@@ -2,5 +2,5 @@ var jj=new Map([
 ["firstarticle",
 "https://github.com/wangbz09/wangbz09.github.io/blob/master/firstarticle.md"],
 ["secondarticle",
-"https://github.com/wangbz09/wangbz09.github.io/blob/master/firstarticle.md"]
+"https://github.com/wangbz09/wangbz09.github.io/blob/master/secondarticle.md"]
 ])
