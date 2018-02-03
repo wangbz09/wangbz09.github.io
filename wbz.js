d4c1f58eaa29186f94e52ab28e3f4e474f325019
wangbz09 <wangbz@189.cn>
2018-02-04 07:25:00 +0800

    fix the second link

--- wbz.js
+++ wbz.js
@@ -1,6 +1,7 @@
 var jj=new Map([
-["firstarticle",
-"https://github.com/wangbz09/wangbz09.github.io/blob/master/firstarticle.md"],
-["secondarticle",
-"https://github.com/wangbz09/wangbz09.github.io/blob/master/secondarticle.md"]
+["2018-03-05 secondarticle",
+"https://github.com/wangbz09/wangbz09.github.io/blob/master/secondarticle.md"],
+["2018-01-03 firstarticle",
+"https://github.com/wangbz09/wangbz09.github.io/blob/master/firstarticle.md"]
+
 ])
