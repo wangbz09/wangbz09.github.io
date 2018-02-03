f8b568150e1619b23f322731b97c3f7d34220fc3
wangbz09 <wangbz@189.cn>
2018-02-04 07:09:45 +0800

    this is pubilished

--- wbz.js
+++ wbz.js
@@ -1,1 +1,6 @@
-var jj=new Map([["hellowbz",0],["hellowbz1",1]]);
+var jj=new Map([
+["firstarticle",
+"https://github.com/wangbz09/wangbz09.github.io/blob/master/firstarticle.md"],
+["secondarticle",
+"https://github.com/wangbz09/wangbz09.github.io/blob/master/firstarticle.md"]
+])
