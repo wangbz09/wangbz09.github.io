e148215347d214310e09e3d5d6a1973b88737fe1
wangbz09 <wangbz@189.cn>
2018-02-18 18:20:29 +0800

    add qsort second wbz.js okok

---
+++ qsort_publish/wbz.js
@@ -0,0 +1,8 @@
+var jj=new Map([
+["4.md ",
+"https://github.com/wangbz09/wangbz09.github.io/blob/master/4.md "],
+["5.md ",
+"https://github.com/wangbz09/wangbz09.github.io/blob/master/5.md "],
+["快速排序算法--分步演示 ",
+"http://www.yiyuntong123.com/qsort_publish/qsort.html "],
+])
