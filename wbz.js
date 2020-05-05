17f392e240a888fbeca86a9f92925418f105c52c
unknown <wangbz@gd.ctc.com>
2020-05-05 11:09:54 +0800

    repair wbz.js link

--- wbz.js
+++ wbz.js
@@ -1,8 +1,5 @@
 var jj = new Map([
   ["4.md ", "https://github.com/wangbz09/wangbz09.github.io/blob/master/4.md "],
   ["5.md ", "https://github.com/wangbz09/wangbz09.github.io/blob/master/5.md "],
-  [
-    "快速排序算法--分步演示 ",
-    "https://github.com/wangbz09/wangbz09.github.io/qsort_publish/qsort.html ",
-  ],
+  ["快速排序算法--分步演示 ", "qsort_publish/qsort.html "],
 ]);
